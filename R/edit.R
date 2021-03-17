8b1cb42f2724ff2641ee5fbea067f4aa62bfd2e7
Nicholas Potter <potterzot@gmail.com>
2021-03-17 08:26:35 -0700

    add gpx test data and package structure.

---
+++ R/edit.R
@@ -0,0 +1,7 @@
+#' Fix common HRM error of inflated heart rate in early part of activity.
+#' 
+fix_hr <- function(max_hr, time_limit = NULL,
+                   infile = NULL,
+                   outfile = NULL) {
+  
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rfit)
+
+test_check("rfit")
