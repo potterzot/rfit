fb45ccc8d3340112b57420d73ff2ac9996b2de56
Nicholas Potter <potterzot@gmail.com>
2021-03-17 09:27:09 -0700

    initial fix_hr function and tests.

--- R/edit.R
+++ R/edit.R
@@ -1,7 +1,43 @@
 #' Fix common HRM error of inflated heart rate in early part of activity.
 #' 
-fix_hr <- function(max_hr, time_limit = NULL,
+fix_hr <- function(hr_base, hr_ceiling, max_minutes = NULL,
                    infile = NULL,
-                   outfile = NULL) {
+                   outfile = NULL,
+                   return_output = FALSE) {
+
+  lines <- readLines(infile)
   
+  # Get start and end times
+  time_idx <- which(grepl("time", lines))
+  start_time <- lubridate::as_datetime(trimws(lines[[ time_idx[[1]] ]]), 
+                                       format = "<time>%Y-%m-%dT%H:%M:%S+00:00</time>")
+  end_time <- lubridate::as_datetime(trimws(lines[[ time_idx[[length(time_idx)]] ]]), 
+                                     format = "<time>%Y-%m-%dT%H:%M:%S+00:00</time>")
+  if(is.null(max_seconds)) max_seconds <- end_time - start_time + 1
+
+  # Loop through each line, setting HR to hr_base if it exceeds hr_max, and 
+  # stopping once max_seconds has passed
+  this_time <- start_time
+  i <- 1
+  while( (this_time - start_time < lubridate::minutes(max_minutes)) & (i <= length(lines))) {
+    if(grepl("<time>", lines[[i]])) {     #set the time of this observation
+      this_time <- lubridate::as_datetime(trimws(lines[[i]]), 
+                                          format = "<time>%Y-%m-%dT%H:%M:%S+00:00</time>")
+    } else if(grepl("<gpxtpx:hr>", lines[[i]])) {
+      hr <- stringr::str_extract(lines[[i]], "\\d+")
+      if(as.numeric(hr) > hr_ceiling) {
+        lines[[i]] <- gsub("\\d+", hr_base, lines[[i]])
+      }
+    }
+    i <- i + 1
+  }
+  
+  if(!is.null(outfile)) { 
+    writeLines(lines, outfile) 
+  }
+  
+  if(return_output) {
+    res <- lines
+  } else res <- NULL
+  res
 }
--- tests/testthat/test-edit.R
+++ tests/testthat/test-edit.R
@@ -1,3 +1,27 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("Editing fitness files.")
+
+test_that("fix_hr", {
+  hr_base <- 130
+  max_seconds <- 28*60
+
+  test_data <- readLines("tests/test.gpx")
+  
+  # Test that hr_ceiling makes second observation not affected
+  d <- fix_hr(hr_base = 130, hr_ceiling = 150, max_minutes = 10, 
+              infile = "tests/test.gpx",
+              return_output = TRUE)
+  expect_equal(trimws(d[grepl("gpxtpx:hr", d)][[2]]), "<gpxtpx:hr>140</gpxtpx:hr>")
+
+  # Test that time limit omits the third observation
+  d <- fix_hr(hr_base = 130, hr_ceiling = 150, max_minutes = 10, 
+         infile = "tests/test.gpx",
+         return_output = TRUE)
+  expect_equal(trimws(d[grepl("gpxtpx:hr", d)][[3]]), "<gpxtpx:hr>190</gpxtpx:hr>")
+
+  # Test that null time limit fixes the whole file
+  d <- fix_hr(hr_base = 130, hr_ceiling = 150, 
+              infile = "tests/test.gpx",
+              return_output = TRUE)
+  expect_equal(trimws(d[grepl("gpxtpx:hr", d)][[3]]), "<gpxtpx:hr>130</gpxtpx:hr>")
+  
 })
